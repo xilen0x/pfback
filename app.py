908be80cea4c12fb5ae23ec909a95aa1ba373b4b
jose armando carballo <jacl12592@gmail.com>
2020-04-30 21:25:06 -0400

    ultimo bakc

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
+import os
 from flask import Flask, jsonify, request, render_template
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
 from flask_jwt_extended import (JWTManager, jwt_required, create_access_token, get_jwt_identity)
-from models import db, User
+from werkzeug.utils import secure_filename
+from models import db, User , Blog , Comment
+from datetime import date , time
+from functions import allowed_file
 
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+ALLOWED_EXTENSIONS_IMAGES = {'png', 'jpg', 'jpeg', 'gif', 'svg'}
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -14,6 +21,7 @@ app.config['ENV'] = 'development'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 app.config['JWT_SECRET_KEY'] = 'super-secret'  # Cambiar luego!!!
+app.config['UPLOAD_FOLDER'] = os.path.join(BASE_DIR, 'static')
 jwt = JWTManager(app)
 
 db.init_app(app)
@@ -24,10 +32,145 @@ manager.add_command("db", MigrateCommand)
 
 CORS(app)
 
-""" rutas """
+
+
 @app.route('/')
 def main():
     return render_template('index.html')
+
+
+
+@app.route('/blog', methods=['GET', 'POST'])
+@app.route('/blog/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+def blog(id = None):
+    if request.method == 'GET':
+
+        if id is not None :
+            blog = Blog.query.get(id)
+            if blog:
+               return jsonify(blog.serialize()), 200
+            else:
+               return jsonify({"msg": "Not Found"}), 404   
+        else :
+            blogs = Blog.query.all()
+            blogs = list(map(lambda blog: blog.serialize(), blogs))
+            return jsonify(blogs), 200
+
+    if request.method == 'POST':
+        
+        e_titulo = request.form.get('e_titulo', None)
+        e_cuerpo = request.form.get('e_cuerpo', None)
+        e_fecha = request.form.get('e_fecha', None)
+       
+              
+        if not e_titulo and e_titulo == "":
+            return jsonify({"msg":"Field titulo is required"}), 400
+        if not e_cuerpo and e_cuerpo == "":
+            return jsonify({"msg":"Field cuerpo is required"}), 400
+        if not e_fecha and e_fecha == "":
+            return jsonify({"msg":"Field fecha is required"}), 400
+
+        file = request.files['e_imagen'] 
+        if file and file.filename !='' and allowed_file(file.filename, ALLOWED_EXTENSIONS_IMAGES):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(os.path.join(app.config['UPLOAD_FOLDER'], 'images/blogs'), filename)) 
+        else:
+            return jsonify({"msg":"File not allowed"}), 400
+        blog = Blog()
+         
+        blog.e_titulo = e_titulo 
+        blog.e_cuerpo = e_cuerpo 
+        blog.e_fecha = e_fecha
+        if file:
+            blog.e_imagen = filename
+
+        db.session.add(blog) 
+        db.session.commit()  
+
+        return jsonify(blog.serialize()), 201
+
+    if request.method == 'PUT':
+        e_titulo = request.form.get('e_titulo', None)
+        e_cuerpo = request.form.get('e_cuerpo', None)
+        e_fecha = request.form.get('e_fecha', None)
+       
+              
+        if not e_titulo and e_titulo == "":
+            return jsonify({"msg":"Field titulo is required"}), 400
+        if not e_cuerpo and e_cuerpo == "":
+            return jsonify({"msg":"Field cuerpo is required"}), 400
+        if not e_fecha and e_fecha == "":
+            return jsonify({"msg":"Field fecha is required"}), 400
+
+        file = request.files['e_imagen'] 
+        if file and file.filename !='' and allowed_file(file.filename, ALLOWED_EXTENSIONS_IMAGES):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(os.path.join(app.config['UPLOAD_FOLDER'], 'images/blogs'), filename)) 
+        else:
+            return jsonify({"msg":"File not allowed"}), 400
+
+        blog = Blog.query.get(id) 
+
+        blog.e_titulo = e_titulo 
+        blog.e_cuerpo = e_cuerpo 
+        blog.e_fecha = e_fecha
+        if file:
+            blog.e_imagen = filename
+ 
+        db.session.commit()
+        return jsonify(blog.serialize()), 201   
+        
+    if request.method == 'DELETE':
+        blog = Blog.query.get(id)
+        if not blog:
+            return jsonify({"msg": "Not Found"}), 404
+        db.session.delete(blog)
+        db.session.commit()
+        return jsonify({"msg":"Blog was deleted"}), 200       
+
+@app.route('/coment', methods=['GET', 'POST'])
+@app.route('/coment/<int:id>', methods=['GET', 'DELETE'])
+def comment(id = None):
+    if request.method == 'GET':
+        
+        if id is not None:
+         pass
+        else :
+         coments = Comment.query.all()
+         coments = list(map(lambda coment: coment.serialize(), coments))
+         return jsonify(coments), 200
+
+    if request.method == 'POST':
+        if not request.is_json:
+          return jsonify({"msg": "No es un objeto JSON"}), 400
+        
+        
+        
+        c_cuerpo = request.json.get('c_cuerpo', None)
+        c_fecha = request.json.get('c_fecha', None)
+        id_blog = request.json.get('id_blog', None)
+        
+        
+        if not c_cuerpo and c_cuerpo == "":
+            return jsonify({"msg":"Field coment is required"}), 400
+        if not c_fecha and c_fecha == "":
+            return jsonify({"msg":"Field fecha is required"}), 400
+        if not id_blog and id_blog == "":
+            return jsonify({"msg":"Field id_blog is required"}), 400    
+
+        coment = Comment()
+        
+        
+        coment.c_cuerpo = c_cuerpo
+        coment.c_fecha = c_fecha
+        coment.id_blog = id_blog
+
+        db.session.add(coment)
+        db.session.commit()
+        return jsonify(coment.serialize()),201    
+
+    if request.method == 'DELETE':
+        return jsonify({"msg": "users delete"}), 200
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -98,7 +241,7 @@ def register():
 @jwt_required # llamando a jwt_required le indico q las rutas abajo son privadas y requiere autorización pra acceder
 def users(id = None):
     if request.method == 'GET':
-        return jsonify({"msg": "users get"}), 200
+        return jsonify({"msg": "usuario get"}), 200
     if request.method == 'POST':
         return jsonify({"msg": "users post"}), 200
     if request.method == 'PUT':
--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import date , time
 db = SQLAlchemy()
 
 class User(db.Model):
@@ -8,6 +9,7 @@ class User(db.Model):
     password = db.Column(db.String(100), nullable=False)
     name = db.Column(db.String(100), nullable=True)
     lastname = db.Column(db.String(100), nullable=True)
+    
 
     def serialize(self):
         return {
@@ -16,3 +18,39 @@ class User(db.Model):
             "name": self.name,
             "lastname": self.lastname
         }
+
+class Blog(db.Model):
+    __tablename__='blog'
+    id_entrada = db.Column(db.Integer, primary_key=True)
+    e_titulo = db.Column(db.String(100), nullable=False)
+    e_cuerpo = db.Column(db.String(500), nullable=False)
+    e_imagen = db.Column(db.String(250), nullable=True, default='vista.jpg')
+    e_fecha = db.Column(db.String(200), nullable=False)
+    
+    
+    def serialize(self):
+	    return {
+            "id_entrada":self.id_entrada,	
+            "e_titulo":self.e_titulo,
+            "e_cuerpo":self.e_cuerpo,
+            "e_imagen":self.e_imagen,
+            "e_fecha":self.e_fecha
+	}
+    
+    
+class Comment(db.Model):
+    __tablename__='comentarios'
+    id_comentario = db.Column(db.Integer, primary_key=True)
+    c_cuerpo = db.Column(db.String(500), nullable=False)
+    c_fecha = db.Column(db.String(200), nullable=False)
+    blog = db.relationship(Blog)
+    id_blog = db.Column(db.String, db.ForeignKey('blog.id_entrada'), nullable=False)#<--------- no estoy seguro pues ya tengo una variable id_entrada en la linea 103.
+    
+    
+    def serialize(self):
+	    return {
+            "id_comentario":self.id_comentario,
+            "c_cuerpo":self.c_cuerpo,
+            "c_fecha":self.c_fecha,
+            "blog":self.blog.serialize(),
+	}           
